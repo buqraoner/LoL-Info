867034b37c61269484a5c080e4bcded19ff0dbd7
Buğra Öner <boletroy@gmail.com>
2022-04-06 23:12:33 +0300

    Search

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,28 @@
 import React from 'react'
 import 'flowbite';
 import Navbar from '../components/Navbar';
+import "./styles.css"
+import Githubdata from '../components/github/Githubdata';
+import PlayerSearcher from '../components/PlayerSearch/PlayerSearch';
 
+
+
+  //Navbar,Oyuncu arama ve 
+  //kendi bilgilerimin bulunduğu github data'sının bulunduğu footer'ının bulunduğu home.jsx'i
 
 function Home() {
-    return (
-      <div>
-          <Navbar/>
-      </div>
-       
-    )
+  return (
+    <div>
+      <Navbar />
+      <PlayerSearcher/>
+      
+      
+      <Githubdata/>
+      
+      
+    </div>
+
+  )
 }
 
 export default Home
