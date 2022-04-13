01c85e56982bcbed60306431ea1af2ae26a1dd1d
Buğra Öner <boletroy@gmail.com>
2022-04-13 23:34:23 +0300

    Footer güncellendi

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ function Footer({ username, avatar, name, url }) {
         <footer className="p-4 bg-gray-800 rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-800 mt-20" >
             <div className="sm:flex sm:items-center sm:justify-between">
                 <a href={url} className="flex items-center mb-4 sm:mb-0">
-                    <img src={avatar} className="mr-3 h-8" alt="Flowbite Logo" />
+                    <img src={avatar} className="mr-3 h-8" alt="Avatar" />
                     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">{name}</span>
                 </a>
                 <ul className="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
@@ -26,8 +26,9 @@ function Footer({ username, avatar, name, url }) {
                 </ul>
             </div>
             <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-            <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href={url} class="hover:underline">React</a> bilgimi geliştirmek için yaptığım proje.
-            </span>
+            <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 
+            <a href={url} class="hover:underline"><strong> React</strong></a> bilgimi geliştirmek için yaptığım proje.
+            </span>                
         </footer>
 
 
